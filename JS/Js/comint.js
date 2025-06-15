5cf537e1aed1d0a86db5ace0eb4a95e20c33fc4d
sirajPek <srajpek51@gmail.com>
2025-06-15 17:46:55 +0300

    Js learen

---
+++ JS/Js/comint.js
@@ -0,0 +1,7 @@
+// لا دراج التعلق نستخل الدبل سلاج للسطر الواحد
+// in-line comment
+
+var number = 0; // تعريف متغير
+
+/* this is a 
+multi-line comment */
---
+++ JS/Js/datatypes.js
@@ -0,0 +1,14 @@
+/* Data Types: 
+    indefined , null , boolean , srting, symbol, number, and abject
+*/
+
+let aq; // undefined
+let bb = null; // null
+let cc = true; // boolean
+let d = "Hello"; // string
+let e = Symbol("unique"); // symbol
+let f = 42; // number
+let g = { name: "John", age: 30 }; // object
+
+// لدي ثلاث انواع من التعريفات للمتغيرات
+// var , let , const
